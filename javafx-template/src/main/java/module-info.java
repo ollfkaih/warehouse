d6890d7e1aa1636460709ab19c88516b912d7921
Fredrik Fonn Hansen <fredrfha@stud.ntnu.no>
2021-09-10 07:36:35 +0000

    Fjernet javafx-template og packages-template

--- javafx-template/src/main/java/module-info.java
+++
@@ -1,6 +0,0 @@
-module app {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens app to javafx.graphics, javafx.fxml;
-}
--- packages-template/src/main/java/module-info.java
+++
@@ -1,6 +0,0 @@
-module app {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens ui to javafx.graphics, javafx.fxml;
-}
