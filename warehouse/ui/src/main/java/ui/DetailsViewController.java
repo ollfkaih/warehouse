31da5b8451f9444141e09b8650991b3ca2d69c96
Fredrik <fredrfha>
2021-10-06 11:51:04 +0200

    Added fxid to elements in DetailsView.fxml

--- warehouse/ui/src/main/java/ui/DetailsViewController.java
+++ warehouse/ui/src/main/java/ui/DetailsViewController.java
@@ -1,10 +1,38 @@
 package ui;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
 
 public class DetailsViewController {
     @FXML ScrollPane detailsViewScrollPane;
 
+    // elements
+    @FXML TextField inpName;
+    @FXML TextField inpAmount;
+    @FXML Button btnIncrement;
+    @FXML Button btnDecrement;
+    @FXML Label placementSection;
+    @FXML Label placementRow;
+    @FXML Label placementShelf;
+    @FXML TextField inpOrdinaryPrice;
+    @FXML TextField inpSalesPrice;
+    @FXML TextField inpRetailerPrice;
+    @FXML ComboBox<String> comboBoxSalesTax;
+    @FXML TextField inpPlacementSection;
+    @FXML TextField inpPlacementRow;
+    @FXML TextField inpPlacementShelf;
+    @FXML TextField inpDimensionsLength;
+    @FXML TextField inpDimensionsWidth;
+    @FXML TextField inpDimensionsHeight;
+    @FXML TextField inpWeight;
+    @FXML TextField inpBarcode;
+
     
+    
+
+
 }
