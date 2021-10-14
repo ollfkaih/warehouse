b6b16e371465e3bb98204c33b6fedfbf8d08e8b6
Eik Hvattum Røgeberg <eik.rogeberg@gmail.com>
2021-10-14 10:59:03 +0200

    Improve input validation

---
+++ warehouse/ui/src/main/java/ui/validators/MinLengthValidator.java
@@ -0,0 +1,17 @@
+package ui.validators;
+
+/**
+ * Validates that the input is longer or equal to the minLength.
+ */
+public class MinLengthValidator implements InputValidator {
+  int minLength;
+
+  public MinLengthValidator(int minLength) {
+    this.minLength = minLength;
+  }
+
+  @Override
+  public boolean validateInput(String input) {
+    return input.length() >= minLength;
+  }
+}
---
+++ warehouse/ui/src/main/java/ui/validators/MinValueValidator.java
@@ -0,0 +1,17 @@
+package ui.validators;
+
+/**
+ * Validates that the value is larger or equal to the minValue.
+ */
+public class MinValueValidator implements InputValidator {
+  double minValue;
+
+  public MinValueValidator(double minValue) {
+    this.minValue = minValue;
+  }
+
+  @Override
+  public boolean validateInput(String input) {
+    return Double.parseDouble(input) >= minValue;
+  }
+}
