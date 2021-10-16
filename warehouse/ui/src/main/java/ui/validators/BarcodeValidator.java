c3c9c278b122e2cf2992a1b8b7385f1baee2f168
Eik Hvattum Røgeberg <eik.rogeberg@gmail.com>
2021-10-16 02:48:42 +0200

    Add barcode validator

---
+++ warehouse/ui/src/main/java/ui/validators/BarcodeValidator.java
@@ -0,0 +1,24 @@
+package ui.validators;
+
+/**
+ * Validates that the input is a valid EAN-13 barcode.
+ */
+public class BarcodeValidator implements InputValidator {
+  private static MinLengthValidator minLengthValidator = new MinLengthValidator(13);
+  private static MaxLengthValidator maxLengthValidator = new MaxLengthValidator(13);
+
+  @Override
+  public boolean validateInput(String input) {
+    if (minLengthValidator.validateInput(input) && maxLengthValidator.validateInput(input)) {
+      int total = 0;
+      for (int i = 0; i < 12; i++) {
+        total += Integer.parseInt("" + input.charAt(i)) * (i % 2 == 0 ? 1 : 3);
+      }
+      int checkDigit = 10 - (total % 10);
+      if (String.valueOf(checkDigit).equals("" + input.charAt(12))) {
+        return true;
+      }
+    }
+    return false;
+  }
+}
