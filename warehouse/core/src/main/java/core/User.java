d2999cded9d7a36c0ced62a9c1ab3a4542099aad
Eik Hvattum Røgeberg <eik.rogeberg@gmail.com>
2021-11-04 02:03:43 +0100

    Make User extend Entity

--- warehouse/core/src/main/java/core/User.java
+++ warehouse/core/src/main/java/core/User.java
@@ -9,7 +9,7 @@ import java.security.NoSuchAlgorithmException;
 /**
  * Class for representing one type of user.
  */
-public class User {
+public class User extends Entity<User> {
   String userName;
   String password;
   boolean admin;
@@ -30,6 +30,7 @@ public class User {
       throw new IllegalArgumentException("Username cannot be empty");
     }
     this.userName = userName;
+    notifyUpdated();
   }
 
   public String getPassword() {
@@ -41,6 +42,7 @@ public class User {
       throw new IllegalArgumentException("Password cannot be empty");
     }
     this.password = password;
+    notifyUpdated();
   }
 
   public boolean isAdmin() {
@@ -49,6 +51,7 @@ public class User {
 
   public void setAdmin(boolean admin) {
     this.admin = admin;
+    notifyUpdated();
   }
 
   public static String md5Hash(String password) {
@@ -72,5 +75,10 @@ public class User {
   @Override
   public String toString() {
     return "Username: " + userName + " Password: " + password + " Admin: " + admin;
+  }
+
+  @Override
+  protected User getThis() {
+    return this;
   }
 }
