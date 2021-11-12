73ddb5f13ceda55d5ac7c24e199a7348866410fc
Olav Kihle <olavki@stud.ntnu.no>
2021-11-12 15:35:32 +0100

    Fix various errors and acessibility issues

--- webapp/src/components/ItemDetails.tsx
+++ webapp/src/components/ItemDetails.tsx
@@ -55,42 +55,46 @@ const ItemDetails = ({ item }: IProps) => {
                 <h5>Plassering</h5>
               </Row>
               <Row className="m-0">
-                <Form.Floating align-content-center as={Col}>
+                <Form.Floating as={Col}>
                   <Form.Control
+                    id="section-control"
                     className="bg-secondary"
                     placeholder="AB"
                     value={openItem.section ?? ''}
                     onChange={(e) => changeValue(e, 'section')}
                   />
-                  <label>Seksjon</label>
+                  <label htmlFor="section-control">Seksjon</label>
                 </Form.Floating>
-                <Form.Floating align-content-center as={Col}>
+                <Form.Floating as={Col}>
                   <Form.Control
+                    id="row-control"
                     className="bg-secondary"
                     placeholder="AB"
                     value={openItem.row ?? ''}
                     onChange={(e) => changeValue(e, 'row')}
                   />
-                  <label>Reol</label>
+                  <label htmlFor="row-control">Reol</label>
                 </Form.Floating>
-                <Form.Floating align-content-center as={Col}>
+                <Form.Floating as={Col}>
                   <Form.Control
+                    id="shelf-control"
                     className="bg-secondary"
                     placeholder="AB"
                     value={openItem.shelf ?? ''}
                     onChange={(e) => changeValue(e, 'shelf')}
                   />
-                  <label>Hylle</label>
+                  <label htmlFor="shelf-control">Hylle</label>
                 </Form.Floating>
               </Row>
             </Col>
           </Row>
           <Row className="m-1">
-            <h5>Antall</h5>
+            <label className="h5" htmlFor="amount-control">Antall</label>
           </Row>
           <Row className="m-1">
             <Form.Group as={Col}>
               <Form.Control
+                id="amount-control"
                 className="bg-secondary"
                 placeholder="0"
                 value={openItem.amount ?? ''}
@@ -103,93 +107,101 @@ const ItemDetails = ({ item }: IProps) => {
             <h5>Pris</h5>
           </Row>
           <Row className="m-1">
-            <Form.Floating align-content-center as={Col}>
+            <Form.Floating as={Col}>
               <Form.Control
+                id="regularPrice-control"
                 className="bg-secondary"
                 placeholder="0.0"
                 value={openItem.regularPrice ?? ''}
                 type="text"
                 onChange={(e) => changeValue(e, 'regularPrice')}
               />
-              <label>Ordinær pris</label>
+              <label htmlFor="regularPrice-control">Ordinær pris</label>
             </Form.Floating>
-            <Form.Floating align-content-center as={Col}>
+            <Form.Floating as={Col}>
               <Form.Control
+                id="salePrice-control"
                 className="bg-secondary"
                 placeholder="0.0"
                 value={openItem.salePrice ?? ''}
                 type="text"
                 onChange={(e) => changeValue(e, 'salePrice')}
               />
-              <label>Salgspris</label>
+              <label htmlFor="salePrice-control">Salgspris</label>
             </Form.Floating>
-            <Form.Floating align-content-center as={Col}>
+            <Form.Floating as={Col}>
               <Form.Control
+                id="purchasePrice-control"
                 className="bg-secondary"
                 placeholder="0.0"
                 value={openItem.purchasePrice ?? ''}
                 type="text"
                 onChange={(e) => changeValue(e, 'purchasePrice')}
               />
-              <label>Innkjøpspris</label>
+              <label htmlFor="purchasePrice-control">Innkjøpspris</label>
             </Form.Floating>
           </Row>
           <Row className="m-1">
             <h5>Dimensjoner</h5>
           </Row>
           <Row className="m-1">
-            <Form.Floating align-content-center as={Col}>
+            <Form.Floating as={Col}>
               <Form.Control
+                id="length-control"
                 className="bg-secondary"
                 placeholder="0.0"
                 value={openItem.length ?? ''}
                 type="text"
                 onChange={(e) => changeValue(e, 'length')}
               />
-              <label>Lengde</label>
+              <label htmlFor="length-control">Lengde</label>
             </Form.Floating>
-            <Form.Floating align-content-center as={Col}>
+            <Form.Floating as={Col}>
               <Form.Control
+                id="width-control"
                 className="bg-secondary"
                 placeholder="0.0"
                 value={openItem.width ?? ''}
                 type="text"
                 onChange={(e) => changeValue(e, 'width')}
               />
-              <label>Bredde</label>
+              <label htmlFor="width-control">Bredde</label>
             </Form.Floating>
-            <Form.Floating align-content-center as={Col}>
+            <Form.Floating as={Col}>
               <Form.Control
+                id="height-control"
                 className="bg-secondary"
                 placeholder="0.0"
                 value={openItem.height ?? ''}
                 type="text"
                 onChange={(e) => changeValue(e, 'height')}
               />
-              <label>Høyde</label>
+              <label htmlFor="height-control">Høyde</label>
             </Form.Floating>
           </Row>
           <Row className="m-1">
             <h5>Vekt</h5>
           </Row>
           <Row className="m-1">
-            <Form.Floating md="4" align-content-center as={Col}>
+            <Form.Floating as={Col}>
               <Form.Control
+                id="weight-control"
                 className="bg-secondary"
                 placeholder="0.0"
                 value={openItem.weight ?? ''}
                 type="text"
                 onChange={(e) => changeValue(e, 'weight')}
               />
-              <label>kg</label>
+              <label htmlFor="weight-control">kg</label>
             </Form.Floating>
           </Row>
           <Row className="m-1">
-            <h5>Strekkode</h5>
+            <label className="h5" htmlFor="barcode-control">Strekkode</label>
           </Row>
           <Row className="m-1">
             <Form.Group as={Col}>
               <Form.Control
+                id="barcode-control"
                 className="bg-secondary"
                 placeholder="0360002914522"
                 value={openItem.barcode ?? ''}
