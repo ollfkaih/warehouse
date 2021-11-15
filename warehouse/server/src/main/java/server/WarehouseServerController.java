7e042bf75c8a4b3c8b9380f48dc3636b7f97318c
Eik Hvattum Røgeberg <eik.rogeberg@gmail.com>
2021-11-15 19:52:20 +0100

    Fix cross-origin setting

--- warehouse/server/src/main/java/server/WarehouseServerController.java
+++ warehouse/server/src/main/java/server/WarehouseServerController.java
@@ -2,10 +2,10 @@ package server;
 
 import core.Item;
 import core.ServerWarehouse;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +16,7 @@ import java.util.Collection;
 /**
  * Contains the main REST-API methods.
  */
+@CrossOrigin
 @RestController
 @RequestMapping(WarehouseServerController.WAREHOUSE_SERVICE_PATH)
 public class WarehouseServerController {
