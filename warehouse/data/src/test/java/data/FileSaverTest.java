38159342194c1ca09d51ec271fc14f55003a559f
Olav Kihle <olavki@stud.ntnu.no>
2021-11-26 16:53:40 +0100

    Added file saving test

---
+++ warehouse/data/src/test/java/data/FileSaverTest.java
@@ -0,0 +1,31 @@
+package data;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import core.main.Item;
+import core.main.User;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+class FileSaverTest {
+  @Test
+  @DisplayName("Test that we can write and read items and users from file")
+  void testSerializeDeserialize() throws IOException {
+    Item item = new Item("Bok", 4);
+    User user = new User("test87", "password", false);
+
+    FileSaver<Item> itemSaver = new FileSaver<Item>(new TypeReference<Item>() {}, "test-items-folder");
+    itemSaver.save(item, "testItem");
+    FileSaver<User> userSaver = new FileSaver<User>(new TypeReference<User>() {}, "test-user-folder");
+    userSaver.save(user, "testUser");
+
+    Item loadedItem = itemSaver.load("testItem");
+    User loadedUser = userSaver.load("testUser");
+
+    assertEquals(item, loadedItem);
+    assertEquals(user, loadedUser);
+  }
+}
